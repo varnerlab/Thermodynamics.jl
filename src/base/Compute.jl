a3b96e69f6f8149f67e46650047883d86ad02b6a
varnerlab <jdv27@cornell.edu>
2020-06-06 12:04:09 -0400

    Update Compute.jl
    Working on the LK impl, finished the PR impl

--- src/base/Compute.jl
+++ src/base/Compute.jl
@@ -23,6 +23,63 @@ function obj_function_volume_vdw(x, eos::VanDerWaalsEquationOfState, P::Float64,
     # return -
     return (err*err) + 10000.0*(p_term_1 + p_term_2)
 end
+
+function obj_function_volume_pr(x, eos::PengRobinsonEquationOfState, P::Float64, T::Float64)
+
+    # get parameters from eos -
+    R = eos.R
+    a = eos.a
+    b = eos.b
+    𝞳 = eos.𝞳
+    Tc = eos.Tc
+
+    # get x values -
+    V = x[1]
+  
+    # compute terms -
+    Tr = T/Tc
+    𝜶 = (1+𝞳*(1-Tr^0.5))^2
+    repulsion = (R*T)/(V-b)
+    attraction = (a*𝜶)/(V^2+2*b*V-b^2)
+
+    # compute error -
+    err = (P - repulsion + attraction)
+
+    # penalty terms -
+    p_term_1 = max(0,-1*V)
+    p_term_2 = max(0,-1*(V-b))
+ 
+    # return -
+    return (err*err) + 10000.0*(p_term_1 + p_term_2)
+end
+
+function obj_function_temperature_pr(x, eos::PengRobinsonEquationOfState, P::Float64, V::Float64)
+
+    # get parameters from eos -
+    R = eos.R
+    a = eos.a
+    b = eos.b
+    𝞳 = eos.𝞳
+    Tc = eos.Tc
+
+    # get x values -
+    T = x[1]
+  
+    # compute terms -
+    Tr = T/Tc
+    𝜶 = (1+𝞳*(1-Tr^0.5))^2
+    repulsion = (R*T)/(V-b)
+    attraction = (a*𝜶)/(V^2+2*b*V-b^2)
+
+    # compute error -
+    err = (P - repulsion + attraction)
+
+    # penalty terms -
+    p_term_1 = max(0,-1*T)
+ 
+    # return -
+    return (err*err) + 10000.0*(p_term_1)
+end
 # -------------------------------------------------------------------------- #
 
 # --- Ideal gas EOS ------------------------------------------------------------------------------- #
@@ -149,10 +206,30 @@ function compute_pressure_pr(eos::PengRobinsonEquationOfState, V::Float64, T::Fl
     return (repulsion - attraction)
 end
 
-function compute_volume_pr(eos::PengRobinsonEquationOfState, P::Float64, T::Float64)::Float64
+function compute_volume_pr(eos::PengRobinsonEquationOfState, P::Float64, T::Float64;  V::Float64 = 1.0)::Float64
+
+    # setup the calculation -
+    xinitial = [V]
+    OF(p) = obj_function_volume_pr(p, eos, P, T)
+    
+    # call the optimizer -
+    opt_result = optimize(OF,xinitial,BFGS())
+    
+    # return -
+    return Optim.minimizer(opt_result)[1]
 end
 
-function compute_temperature_pr(eos::PengRobinsonEquationOfState, P::Float64, V::Float64)::Float64
+function compute_temperature_pr(eos::PengRobinsonEquationOfState, P::Float64, V::Float64; T::Float64)::Float64
+
+    # setup the calculation -
+    xinitial = [T]
+    OF(p) = obj_function_temperature_pr(p, eos, P, V)
+    
+    # call the optimizer -
+    opt_result = optimize(OF,xinitial,BFGS())
+    
+    # return -
+    return Optim.minimizer(opt_result)[1]
 end
 
 # --- Martin-Hou EOS ----------------------------------------------------------------------------- #
@@ -261,6 +338,112 @@ function compute_temperature_mh(eos::MartinHouEquationOfState, P::Float64, V::Fl
 end
 # ------------------------------------------------------------------------- #
 
+# --- Lee Kesler EOS ----------------------------------------------------------------------------- #
+function compute_volume_lk(eos::LeeKeslerEquationOfState, V::Float64, T::Float64)::Float64
+    
+    # get parameters from the model -
+    parameter_dict = eos.parameters
+    R = parameter_dict["R"]
+    A2 = parameter_dict["A2"]
+    B2 = parameter_dict["B2"]
+    C2 = parameter_dict["C2"]
+    A3 = parameter_dict["A3"]
+    B3 = parameter_dict["B3"]
+    C3 = parameter_dict["C3"]
+    A4 = parameter_dict["A4"]
+    B4 = parameter_dict["B4"]
+    C4 = parameter_dict["C4"]
+    A5 = parameter_dict["A5"]
+    B5 = parameter_dict["B5"]
+    C5 = parameter_dict["C5"]
+    b = parameter_dict["b"]
+    k = parameter_dict["k"]
+    Tc = parameter_dict["Tc"]
+
+    # compute reduced T -
+    Tr = T/Tc
+
+    # check - is V = 0
+    if (V==0.0)
+        throw(ArgumentError("Volume == 0: attraction term is singular"))
+    elseif ((V-b) == 0.0)
+        throw(ArgumentError("Volume == b: singular repulsion term"))
+    end
+
+    # compute terms -
+    term_array = Float64[]
+    term_1 = (R*T)/(V-b)
+    term_2 = (A2+B2*T+C2*exp(-k*Tr))/((V-b)^2)
+    term_3 = (A3+B3*T+C3*exp(-k*Tr))/((V-b)^3)
+    term_4 = (A4+B4*T+C4*exp(-k*Tr))/((V-b)^4)
+    term_5 = (A5+B5*T+C5*exp(-k*Tr))/((V-b)^5)
+    
+    # cache -
+    push!(term_array, term_1)
+    push!(term_array, term_2)
+    push!(term_array, term_3)
+    push!(term_array, term_4)
+    push!(term_array, term_5)
+
+    # return -
+    return sum(term_array)
+end
+
+function compute_pressure_lk(eos::LeeKeslerEquationOfState, V::Float64, T::Float64)::Float64
+    
+    # get parameters from the model -
+    parameter_dict = eos.parameters
+    R = parameter_dict["R"]
+    A2 = parameter_dict["A2"]
+    B2 = parameter_dict["B2"]
+    C2 = parameter_dict["C2"]
+    A3 = parameter_dict["A3"]
+    B3 = parameter_dict["B3"]
+    C3 = parameter_dict["C3"]
+    A4 = parameter_dict["A4"]
+    B4 = parameter_dict["B4"]
+    C4 = parameter_dict["C4"]
+    A5 = parameter_dict["A5"]
+    B5 = parameter_dict["B5"]
+    C5 = parameter_dict["C5"]
+    b = parameter_dict["b"]
+    k = parameter_dict["k"]
+    Tc = parameter_dict["Tc"]
+
+    # compute reduced T -
+    Tr = T/Tc
+
+    # check - is V = 0
+    if (V==0.0)
+        throw(ArgumentError("Volume == 0: attraction term is singular"))
+    elseif ((V-b) == 0.0)
+        throw(ArgumentError("Volume == b: singular repulsion term"))
+    end
+
+    # compute terms -
+    term_array = Float64[]
+    term_1 = (R*T)/(V-b)
+    term_2 = (A2+B2*T+C2*exp(-k*Tr))/((V-b)^2)
+    term_3 = (A3+B3*T+C3*exp(-k*Tr))/((V-b)^3)
+    term_4 = (A4+B4*T+C4*exp(-k*Tr))/((V-b)^4)
+    term_5 = (A5+B5*T+C5*exp(-k*Tr))/((V-b)^5)
+    
+    # cache -
+    push!(term_array, term_1)
+    push!(term_array, term_2)
+    push!(term_array, term_3)
+    push!(term_array, term_4)
+    push!(term_array, term_5)
+
+    # return -
+    return sum(term_array)
+end
+
+function compute_temperature_lk(eos::LeeKeslerEquationOfState, P::Float64, V::Float64)::Float64 
+    return 0.0
+end
+# ------------------------------------------------------------------------- #
+
 # --- EXPORTED METHODS ----------------------------------------------------- #
 function pressure(eos::AbstractEquationOfState, V::Float64, T::Float64)::Float64
 
@@ -272,6 +455,8 @@ function pressure(eos::AbstractEquationOfState, V::Float64, T::Float64)::Float64
         return compute_pressure_mh(eos, V, T)
     elseif (typeof(eos) == PengRobinsonEquationOfState)
         return compute_pressure_pr(eos, V, T)
+    elseif (typeof(eos) == LeeKeslerEquationOfState)
+        return compute_pressure_lk(eos, V, T)
     else
         throw(ArgumentError("$(typeof(eos)) is not supported"))
     end
@@ -287,6 +472,8 @@ function volume(eos::AbstractEquationOfState, P::Float64, T::Float64)::Float64
         return compute_volume_mh(eos, P, T)
     elseif (typeof(eos) == PengRobinsonEquationOfState)
         return compute_volume_pr(eos, P, T)
+    elseif (typeof(eos) == LeeKeslerEquationOfState)
+        return compute_volume_lk(eos, P, T)
     else
         throw(ArgumentError("$(typeof(eos)) is not supported"))
     end
@@ -301,6 +488,8 @@ function temperature(eos::AbstractEquationOfState,P::Float64,V::Float64)::Float6
         return compute_temperature_mh(eos, P, V)
     elseif (typeof(eos) == PengRobinsonEquationOfState)
         return compute_temperature_pr(eos, P, V)
+    elseif (typeof(eos) == LeeKeslerEquationOfState)
+        return compute_temperature_lk(eos, P, V)
     else
         throw(ArgumentError("$(typeof(eos)) is not supported"))
     end
